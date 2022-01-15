607ea90817f09f12c6f6c1c9fc1c3f9a813d061f
Michal Bobuš <michal.bobus@gmail.com>
2022-01-16 00:16:59 +0100

    merge txt files mess :)

---
+++ GtaGxtTool/Model/TxtEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtaGxtTool.Model
+{
+    public class TxtEntry
+    {
+        public string Value;
+    }
+}
